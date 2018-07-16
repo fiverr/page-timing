40cc223e198593c5314cd69daffb4661cf57a5d6
Omri <516342+omrilotan@users.noreply.github.com>
2018-07-16 11:42:14 +0100

    Create a fallback for systems where WeakMap is not available (#3)
    Create a fallback for systems where WeakMap is not available - When no weakmap exists, don't memoise

--- src/memoise/index.js
+++ src/memoise/index.js
@@ -1,9 +1,13 @@
-const mem = new WeakMap();
-
 /**
  * Memoises value by reference
  * @param  {Object}   key
  * @param  {Function} fn
  * @return {Any}
  */
-export const memoise = (key, fn) => mem.has(key) ? mem.get(key) : mem.set(key, fn()).get(key);
+export const memoise = (() => {
+    if (typeof WeakMap !== 'function') {
+        return (key, fn) => fn();
+    }
+    const mem = new WeakMap();
+    return (key, fn) => mem.has(key) ? mem.get(key) : mem.set(key, fn()).get(key);
+})();
--- src/memoise/spec.js
+++ src/memoise/spec.js
@@ -1,4 +1,5 @@
 describe('memoise', () => {
+    const weakMap = global.WeakMap;
     let memoise;
     const key = {};
 
@@ -7,19 +8,18 @@ describe('memoise', () => {
         memoise = require('.').memoise;
     });
 
-    it('Should uses the getter to retrieve the value', () => {
-        memoise(key, () => 'Thang');
-        expect(memoise(key)).to.equal('Thang');
+    after(() => {
+        global.WeakMap = weakMap;
     });
 
-    it('Should return the value', () =>
-        expect(memoise(key, () => 'Thang')).to.equal('Thang')
+    it('Should uses the getter to retrieve the value', () =>
+        expect(memoise({}, () => 'Thang')).to.equal('Thang')
     );
 
     it('Should memoise the result', () => {
         memoise(key, () => 'Thing');
-        memoise(key, () => 'Thang');
-        expect(memoise(key)).to.equal('Thing');
+        expect(memoise(key, () => 'Thang')).to.equal('Thing');
+        expect(memoise(key, () => 'Thong')).to.equal('Thing');
     });
 
     it('Should throw error when trying to memoise primitives', () =>
@@ -29,4 +29,14 @@ describe('memoise', () => {
             4,
         ].forEach((key) => expect(() => memoise(key, () => 'Thing')).to.throw())
     );
+
+    it('Should fall back to using an object when weakmap is not available', () => {
+        global.WeakMap = null;
+        delete require.cache[require.resolve('.')];
+        memoise = require('.').memoise;
+
+        expect(() => memoise(key, () => 'Thang')).not.to.throw();
+        expect(memoise(key, () => 'Thang')).to.equal('Thang');
+        expect(memoise(key, () => 'Thong')).not.to.equal('Thang');
+    });
 });
