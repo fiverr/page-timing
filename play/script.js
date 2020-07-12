2a0d6f97c3d81967a8b2f618f10b079ff5fb1875
omrilotan <omri@hamadgera.com>
2020-07-12 15:04:45 +0000

    I think now it works

--- play/script.js
+++
@@ -1,7 +0,0 @@
-import { getLCP, getFID, getCLS } from 'web-vitals';
-import { measure, fps, all } from '../src/index.js';
-
-Object.assign(
-    window,
-    { measure, fps, all, getLCP, getFID, getCLS }
-);
--- src/snake-case/index.js
+++
@@ -1,9 +0,0 @@
-/**
- * @param {string}
- * @returns {string}
- */
-export const snakeCase = (str) => str
-    .replace(/([A-Z])/g, '_$1')
-    .replace(/-/g, '_')
-    .toLowerCase()
-    .replace(/_j_s_/g, '_js_');
