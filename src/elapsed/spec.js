9f424ea81bc8f6cf470fbc2232c3d0e16a273d0f
omrilotan <omri@hamadgera.com>
2020-10-08 12:49:08 +0300

    undefined gets omitted on JSON

--- src/elapsed/spec.js
+++ src/elapsed/spec.js
@@ -1,9 +1,24 @@
 import { elapsed } from '.';
 
+const { now } = window.performance;
+
 describe('elapsed', () => {
+    afterEach(() => {
+        window.performance.now = now;
+    });
     it('should returned the time elapsed since the time origin', () => {
         const { page_time_elapsed } = elapsed();
         expect(page_time_elapsed).to.be.a('number');
         expect(page_time_elapsed).to.be.at.least(50);
     });
+    [
+        null,
+        0,
+    ].forEach(
+        (item) => it('should return "undefined" if falsy', () => {
+            window.performance.now = () => item;
+            const { page_time_elapsed } = elapsed();
+            expect(page_time_elapsed).to.equal(undefined);
+        })
+    );
 });
