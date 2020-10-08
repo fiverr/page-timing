f1d5a5987fd7b2d61f3c091385c2c760c8c58144
omrilotan <516342+omrilotan@users.noreply.github.com>
2020-10-08 13:48:55 +0300

    2020 10 08 page time elapsed null (#24)

--- src/elapsed/index.js
+++ src/elapsed/index.js
@@ -1,3 +1,8 @@
-export const elapsed = () => ({
-    page_time_elapsed: window.performance.now()
-});
+export function elapsed() {
+    const page_time_elapsed = window.performance.now();
+
+    return Number.isFinite(page_time_elapsed)
+        ? { page_time_elapsed }
+        : {}
+    ;
+}
--- src/elapsed/spec.js
+++ src/elapsed/spec.js
@@ -1,9 +1,40 @@
 import { elapsed } from '.';
 
+const { now } = window.performance;
+
 describe('elapsed', () => {
+    afterEach(() => {
+        window.performance.now = now;
+    });
     it('should returned the time elapsed since the time origin', () => {
         const { page_time_elapsed } = elapsed();
         expect(page_time_elapsed).to.be.a('number');
         expect(page_time_elapsed).to.be.at.least(50);
     });
+    [
+        4,
+        0,
+        -10,
+        1e3
+    ].forEach(
+        (value) => it('should return the result of "performance.now" function', () => {
+            window.performance.now = () => value;
+            const { page_time_elapsed } = elapsed();
+            expect(page_time_elapsed).to.equal(value);
+        })
+    );
+    [
+        null,
+        '4',
+        Infinity,
+        NaN
+    ].forEach(
+        (value) => it('should be undefined if not a finite number', () => {
+            window.performance.now = () => value;
+            const result = elapsed();
+            expect(result).to.not.have.key('page_time_elapsed');
+            const { page_time_elapsed } = result;
+            expect(page_time_elapsed).to.equal(undefined);
+        })
+    );
 });
